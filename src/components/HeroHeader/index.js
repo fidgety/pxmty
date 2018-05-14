14823a3788b43ff45be50298bb5d6e537c234172
Simon Chiu <si@sichiu.co.uk>
2018-05-14 20:37:05 +0100

    Hero header updates

--- src/components/HeroHeader/index.js
+++ src/components/HeroHeader/index.js
@@ -16,8 +16,38 @@ const Hero = styled.div`
   padding: 10px 10px 0 10px;
 `;
 
+const Heading = styled.div`
+  display: none;
+  position: absolute;
+  padding-top: 100px;
+  padding-left: 35%;
+  text-align: center;
+
+  @media (min-width: ${breakpoints.small}) {
+    display: block;
+    padding-top: 175px;
+    width: 30%;
+  }
+
+  @media (min-width: ${breakpoints.medium}) {
+    padding-top: 140px;
+    padding-left: 32%;
+    width: 35%;
+  }
+
+  @media (min-width: ${breakpoints.large}) {
+    padding-top: 175px;
+  }
+`;
+
 export default () => (
   <Hero>
     <div className="fb" />
+    <Heading>
+      <h1>
+        Planning to go away? Plot, plan and share your travel itinerary with
+        friends.
+      </h1>
+    </Heading>
   </Hero>
 );
